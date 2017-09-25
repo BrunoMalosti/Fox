a1a52f986192be9206f33b176c43f9cf9dc4f1b7
BrunoMalosti <brunomalost@hotmail.com>
2017-09-25 10:59:33 -0300

    Listando

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-@IonicPage()
+import { EventProvider } from '../../providers/event/event';
+@IonicPage({
+    segment: 'event-detail/:eventId'
+})
 @Component({
-selector: 'page-event-detail',
-templateUrl: 'event-detail.html',
+    selector: 'page-event-detail',
+    templateUrl: 'event-detail.html',
 })
 export class EventDetailPage {
-constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    public currentEvent: any = {};
+    constructor(public navCtrl: NavController, public navParams: NavParams,
+        public eventProvider: EventProvider) { }
+    ionViewDidLoad() {
+        this.eventProvider.getEventDetail(this.navParams.get('eventId'))
+            .on('value', eventSnapshot => {
+                this.currentEvent = eventSnapshot.val();
+                this.currentEvent.id = eventSnapshot.key;
+            });
+    }
+/*    addGuest(guestName: string): void {
+        this.eventProvider.addGuest(guestName, this.currentEvent.id,
+            this.currentEvent.price).then(newGuest => {
+                this.guestName = '';
+            });
+    }
+*/
 }
