fc715751edb9cf71c698d81af4869008eace6efe
BrunoMalosti <brunomalost@hotmail.com>
2017-09-29 11:27:15 -0300

    titulo dos termos

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -14,28 +14,23 @@ export class EventListPage {
   constructor(public navCtrl: NavController, public eventProvider:
     EventProvider, public profileProvider: ProfileProvider) { }
 
-    
-
-
-  ionViewDidLoad() {
+      ionViewDidLoad() {
       this.eventProvider.getEventList().on('value', eventListSnapshot => {
       this.eventList = [];
       eventListSnapshot.forEach(snap => {
-        console.log(eventListSnapshot.toJSON);
         this.eventList.push({
           id: snap.key,
           name: snap.val().name,
           price: snap.val().price,
           datei: snap.val().datei,
           datef: snap.val().datef,
-          Uid: snap.val().UID_DONO
-          
+          Uid: snap.val().UID_DONO,          
         });
         return false;
       });
     });
   }
-
+  
   goToEventDetail(eventId):void {
     this.navCtrl.push('EventDetailPage', { 'eventId': eventId });
     }
